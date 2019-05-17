11bae499c1760664b3da37f924a40ee9b0c5bf07
PeterYegorov <yegorov-2000@yandex.ru>
2019-05-17 20:43:37 +0300

    added comments

--- sources/HelloWorld.cpp
+++ sources/HelloWorld.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main()
 {
-    std::string username;
-    std::cout << "Enter your name:" << std::endl;
-    std::cin >> username;
-    std::cout << "Hello, World! From " << username;
+    std::string username; // Создаем переменную для хранения имени пользователя
+    std::cout << "Enter your name:" << std::endl; // Выводим запрос имени
+    std::cin >> username; // Вводим имя пользователя с клавиатуры
+    std::cout << "Hello, World! From " << username; // Выводим получившееся предложение
 }
