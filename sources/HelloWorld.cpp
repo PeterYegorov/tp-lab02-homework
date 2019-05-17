824778f4fc9d2e31356e7f1ed7ba4d45e1f0d46b
PeterYegorov <43631802+PeterYegorov@users.noreply.github.com>
2019-05-18 00:31:18 +0300

    added dots to create conflicts

--- sources/HelloWorld.cpp
+++ sources/HelloWorld.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main()
 {
-    std::string username; // Создаем переменную для хранения имени пользователя
-    std::cout << "Enter your name:" << std::endl; // Выводим запрос имени
-    std::cin >> username; // Вводим имя пользователя с клавиатуры
-    std::cout << "Hello, World! From " << username; // Выводим получившееся предложение
+    std::string username; // Создаем переменную для хранения имени пользователя.
+    std::cout << "Enter your name:" << std::endl; // Выводим запрос имени.
+    std::cin >> username; // Вводим имя пользователя с клавиатуры.
+    std::cout << "Hello, World! From " << username; // Выводим получившееся предложение.
 }
